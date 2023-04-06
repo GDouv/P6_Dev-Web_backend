e98f79cc0621f8d7220a513e9cdfb3d31744db32
Guillaume Douv <gui.douvrin@gmail.com>
2023-04-06 11:43:15 +0200

    Ajout d'un commentaire

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -2,172 +2,173 @@ const Sauce = require("../models/Sauce");
 const fs = require("fs");
 
 exports.createSauce = (req, res) => {
-    const sauceInput = JSON.parse(req.body.sauce);
-    console.log("createSauce ---> req.body.sauce : ");
-    console.log(req.body.sauce);
-    delete sauceInput._id;
-    delete sauceInput._userId;
-    const sauce = new Sauce({
-        ...sauceInput,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-            req.file.filename
-        }`,
-    });
+	const sauceInput = JSON.parse(req.body.sauce);
+	console.log("createSauce ---> req.body.sauce : ");
+	console.log(req.body.sauce);
+	delete sauceInput._id;
+	delete sauceInput._userId;
+	const sauce = new Sauce({
+		...sauceInput,
+		userId: req.auth.userId,
+		// req.protocol récupère le protocole "http" ou "https" et req.get("host") récupère le début de l'URL ("localhost:4200" par exemple);
+		imageUrl: `${req.protocol}://${req.get("host")}/images/${
+			req.file.filename
+		}`,
+	});
 
-    sauce.save();
-    try {
-        res.status(201).json({ message: "Objet enregistré !" });
-    } catch (err) {
-        res.status(400).json({ err });
-    }
+	sauce.save();
+	try {
+		res.status(201).json({ message: "Objet enregistré !" });
+	} catch (err) {
+		res.status(400).json({ err });
+	}
 };
 
 // ES7
 exports.modifySauce = async (req, res) => {
-    try {
-        const sauceObject = req.file
-            ? {
-                  ...JSON.parse(req.body.sauce),
-                  imageUrl: `${req.protocol}://${req.get("host")}/images/${
-                      req.file.filename
-                  }`,
-              }
-            : { ...req.body };
+	try {
+		const sauceObject = req.file
+			? {
+					...JSON.parse(req.body.sauce),
+					imageUrl: `${req.protocol}://${req.get("host")}/images/${
+						req.file.filename
+					}`,
+			  }
+			: { ...req.body };
 
-        delete sauceObject._userId;
-        const sauce = await Sauce.findById(req.params.id).exec();
-        if (sauce.userId != req.auth.userId) {
-            res.status(403).json({ message: "Unauthorized request." });
-        } else {
-            if (sauceObject.imageUrl != null) {
-                const filename = sauce.imageUrl.split("images/")[1];
-                fs.unlink(`images/${filename}`, (err) => {
-                    if (err) throw err;
-                    console.log("Ancienne image supprimée.");
-                });
-            }
-            try {
-                await Sauce.findByIdAndUpdate(req.params.id, {
-                    ...sauceObject,
-                    _id: req.params.id,
-                }).exec();
-                res.status(200).json({ message: "Objet modifié!" });
-            } catch (err) {
-                res.status(401).json({ err });
-            }
-        }
-    } catch (err) {
-        res.status(400).json({ err });
-    }
+		delete sauceObject._userId;
+		const sauce = await Sauce.findById(req.params.id).exec();
+		if (sauce.userId != req.auth.userId) {
+			res.status(403).json({ message: "Unauthorized request." });
+		} else {
+			if (sauceObject.imageUrl != null) {
+				const filename = sauce.imageUrl.split("images/")[1];
+				fs.unlink(`images/${filename}`, (err) => {
+					if (err) throw err;
+					console.log("Ancienne image supprimée.");
+				});
+			}
+			try {
+				await Sauce.findByIdAndUpdate(req.params.id, {
+					...sauceObject,
+					_id: req.params.id,
+				}).exec();
+				res.status(200).json({ message: "Objet modifié!" });
+			} catch (err) {
+				res.status(401).json({ err });
+			}
+		}
+	} catch (err) {
+		res.status(400).json({ err });
+	}
 };
 
 // ES7
 exports.likeSauce = async (req, res) => {
-    try {
-        const sauce = await Sauce.findById(req.params.id).exec();
-        switch (req.body.like) {
-            case 1:
-                if (!sauce.usersLiked.includes(req.body.userId)) {
-                    try {
-                        await Sauce.findByIdAndUpdate(req.params.id, {
-                            $inc: { likes: 1 },
-                            $push: { usersLiked: req.body.userId },
-                        });
-                        res.status(201).json({ message: "Like = 1" });
-                    } catch (err) {
-                        res.status(400).json({ err });
-                    }
-                }
-                break;
-            case -1:
-                if (!sauce.usersDisliked.includes(req.body.userId)) {
-                    try {
-                        await Sauce.findByIdAndUpdate(req.params.id, {
-                            $inc: { dislikes: 1 },
-                            $push: { usersDisliked: req.body.userId },
-                        });
-                        res.status(201).json({ message: "Like = -1" });
-                    } catch (err) {
-                        res.status(400).json({ err });
-                    }
-                }
-                break;
-            case 0:
-                if (sauce.usersLiked.includes(req.body.userId)) {
-                    try {
-                        await Sauce.findByIdAndUpdate(req.params.id, {
-                            $inc: { likes: -1 },
-                            $pull: { usersLiked: req.body.userId },
-                        });
-                        res.status(201).json({ message: "Like = 0" });
-                    } catch (err) {
-                        res.status(400).json({ err });
-                    }
-                } else if (sauce.usersDisliked.includes(req.body.userId)) {
-                    try {
-                        await Sauce.findByIdAndUpdate(req.params.id, {
-                            $inc: { dislikes: -1 },
-                            $pull: { usersDisliked: req.body.userId },
-                        });
-                        res.status(201).json({ message: "Like = 0" });
-                    } catch (err) {
-                        res.status(400).json({ err });
-                    }
-                }
-                break;
-            default:
-                console.log("Unknown sauce or user");
-        }
-    } catch (err) {
-        res.status(404).json({ err });
-    }
+	try {
+		const sauce = await Sauce.findById(req.params.id).exec();
+		switch (req.body.like) {
+			case 1:
+				if (!sauce.usersLiked.includes(req.body.userId)) {
+					try {
+						await Sauce.findByIdAndUpdate(req.params.id, {
+							$inc: { likes: 1 },
+							$push: { usersLiked: req.body.userId },
+						});
+						res.status(201).json({ message: "Like = 1" });
+					} catch (err) {
+						res.status(400).json({ err });
+					}
+				}
+				break;
+			case -1:
+				if (!sauce.usersDisliked.includes(req.body.userId)) {
+					try {
+						await Sauce.findByIdAndUpdate(req.params.id, {
+							$inc: { dislikes: 1 },
+							$push: { usersDisliked: req.body.userId },
+						});
+						res.status(201).json({ message: "Like = -1" });
+					} catch (err) {
+						res.status(400).json({ err });
+					}
+				}
+				break;
+			case 0:
+				if (sauce.usersLiked.includes(req.body.userId)) {
+					try {
+						await Sauce.findByIdAndUpdate(req.params.id, {
+							$inc: { likes: -1 },
+							$pull: { usersLiked: req.body.userId },
+						});
+						res.status(201).json({ message: "Like = 0" });
+					} catch (err) {
+						res.status(400).json({ err });
+					}
+				} else if (sauce.usersDisliked.includes(req.body.userId)) {
+					try {
+						await Sauce.findByIdAndUpdate(req.params.id, {
+							$inc: { dislikes: -1 },
+							$pull: { usersDisliked: req.body.userId },
+						});
+						res.status(201).json({ message: "Like = 0" });
+					} catch (err) {
+						res.status(400).json({ err });
+					}
+				}
+				break;
+			default:
+				console.log("Unknown sauce or user");
+		}
+	} catch (err) {
+		res.status(404).json({ err });
+	}
 };
 
 //ES7
 exports.deleteSauce = async (req, res) => {
-    try {
-        const sauce = await Sauce.findById(req.params.id).exec();
-        if (sauce.userId != req.auth.userId) {
-            res.status(403).json({ message: "Unauthorized request." });
-        } else {
-            const filename = sauce.imageUrl.split("images/")[1];
-            fs.unlink(`images/${filename}`, async () => {
-                try {
-                    await Sauce.findByIdAndDelete(req.params.id).exec();
-                    res.status(200).json({
-                        message: "Objet supprimé !",
-                    });
-                } catch (err) {
-                    res.status(401).json({ err });
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).json({ err });
-    }
+	try {
+		const sauce = await Sauce.findById(req.params.id).exec();
+		if (sauce.userId != req.auth.userId) {
+			res.status(403).json({ message: "Unauthorized request." });
+		} else {
+			const filename = sauce.imageUrl.split("images/")[1];
+			fs.unlink(`images/${filename}`, async () => {
+				try {
+					await Sauce.findByIdAndDelete(req.params.id).exec();
+					res.status(200).json({
+						message: "Objet supprimé !",
+					});
+				} catch (err) {
+					res.status(401).json({ err });
+				}
+			});
+		}
+	} catch (err) {
+		res.status(500).json({ err });
+	}
 };
 
 // ES7
 exports.getOneSauce = async (req, res) => {
-    try {
-        const sauce = await Sauce.findById(req.params.id).exec(); // Appeler une query sans exec() ni une callback renverrai un thenable et non pas une promise.
-        res.status(200).json(sauce);
-    } catch (err) {
-        res.status(404).json({ err });
-    }
+	try {
+		const sauce = await Sauce.findById(req.params.id).exec(); // Appeler une query sans exec() ni une callback renverrai un thenable et non pas une promise.
+		res.status(200).json(sauce);
+	} catch (err) {
+		res.status(404).json({ err });
+	}
 };
 
 // ECMAScript7 (ES7 : 2016), toujours valable, async/await
 exports.getAllSauces = async (req, res) => {
-    try {
-        const sauces = await Sauce.find({})
-            .select("imageUrl name heat mainPepper") // .select permet de ne charger que les propriétés précisées, ou inversement de ne pas charger "-propriété" (avec un - devant)
-            .exec();
-        res.status(200).json(sauces);
-    } catch (err) {
-        res.status(500).json({ err });
-    }
+	try {
+		const sauces = await Sauce.find({})
+			.select("imageUrl name heat mainPepper") // .select permet de ne charger que les propriétés précisées, ou inversement de ne pas charger "-propriété" (avec un - devant)
+			.exec();
+		res.status(200).json(sauces);
+	} catch (err) {
+		res.status(500).json({ err });
+	}
 };
 
 // ES6
